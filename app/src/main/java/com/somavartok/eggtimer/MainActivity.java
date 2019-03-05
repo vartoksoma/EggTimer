5d502021e1dfb0fbb296e40b61130abdf0541a0d
Vartok Soma <vartok.soma@gmail.com>
2019-03-05 11:32:06 +0100

    added seekbar, and changing the value, start button

--- app/src/main/java/com/somavartok/eggtimer/MainActivity.java
+++ app/src/main/java/com/somavartok/eggtimer/MainActivity.java
@@ -1,13 +1,74 @@
 package com.somavartok.eggtimer;
 
+import android.media.AudioManager;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar = findViewById(R.id.timerSeekBar);
+    TextView timer;
+
+    public void updateTimer(int secondsLeft){
+
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String secondString = Integer.toString(seconds);
+
+        if(secondString.equals("0")){
+            secondString += "0";
+        }
+        timer = findViewById(R.id.timerTextView);
+        timer.setText(Integer.toString(minutes) + ":" + secondString);
+
+    }
+
+    public void count(View view){
+        final
+        CountDownTimer timer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+            @Override
+            public void onTick(long l) {
+                updateTimer((int) l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        }.start();
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(30);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+
+                updateTimer(i);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
