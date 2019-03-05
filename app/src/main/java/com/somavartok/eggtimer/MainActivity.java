44dae397756de2adbbcf880f4d486dc09e67a30b
Vartok Soma <vartok.soma@gmail.com>
2019-03-05 13:07:26 +0100

    small bugfixes, reset method

--- app/src/main/java/com/somavartok/eggtimer/MainActivity.java
+++ app/src/main/java/com/somavartok/eggtimer/MainActivity.java
@@ -1,6 +1,6 @@
 package com.somavartok.eggtimer;
 
-import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,8 +10,11 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    SeekBar seekBar = findViewById(R.id.timerSeekBar);
-    TextView timer;
+    SeekBar seekBar;
+    TextView timerTextView;
+    boolean counterIsActive = false;
+    Button controllerButton;
+    CountDownTimer countDownTimer;
 
     public void updateTimer(int secondsLeft){
 
@@ -20,27 +23,45 @@ public class MainActivity extends AppCompatActivity {
 
         String secondString = Integer.toString(seconds);
 
-        if(secondString.equals("0")){
-            secondString += "0";
+        if(seconds <= 9){
+            secondString = "0" + secondString;
         }
-        timer = findViewById(R.id.timerTextView);
-        timer.setText(Integer.toString(minutes) + ":" + secondString);
+        timerTextView = findViewById(R.id.timerTextView);
+        timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
 
     }
 
+    public void reset(){
+        counterIsActive = false;
+        timerTextView.setText("0:30");
+        seekBar.setProgress(30);
+        seekBar.setEnabled(true);
+        countDownTimer.cancel();
+        controllerButton.setText("Go!");
+    }
+
     public void count(View view){
-        final
-        CountDownTimer timer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
-            @Override
-            public void onTick(long l) {
-                updateTimer((int) l / 1000);
-            }
+        if (!counterIsActive) {
+            counterIsActive = true;
+            seekBar.setEnabled(false);
+            controllerButton.setText("Stop!");
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000 + 100, 1000) {
+                @Override
+                public void onTick(long l) {
+                    updateTimer((int) l / 1000);
+                }
 
-            @Override
-            public void onFinish() {
-
-            }
-        }.start();
+                @Override
+                public void onFinish() {
+                    timerTextView.setText("0:00");
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.horn);
+                    mediaPlayer.start();
+                    reset();
+                }
+            }.start();
+        }else {
+            reset();
+        }
     }
 
 
@@ -48,6 +69,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.timerSeekBar);
+        controllerButton = findViewById(R.id.goButton);
 
         seekBar.setMax(600);
         seekBar.setProgress(30);
